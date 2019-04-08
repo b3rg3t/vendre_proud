8a1d3f91a450d49330fddadfbfad86ea9f4077cf
fabifors <fabian.forsstrom.com>
2019-04-08 11:11:58 +0200

    Fixed linter errors

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,27 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
     <button @click="logout">Logout</button>
   </div>
 </template>
 
 <script>
-import firebase from 'firebase';
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import firebase from "firebase";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
     HelloWorld
   },
   methods: {
     logout: function() {
-      firebase.auth().signOut().then(() => {
-        this.$router.replace('login')
-      })
+      firebase
+        .auth()
+        .signOut()
+        .then(() => {
+          this.$router.replace("login");
+        });
     }
   }
-}
+};
 </script>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,59 +1,80 @@
 <template>
   <div class="login">
     <h3>Sign In</h3>
-    <input type="text" v-model="email" placeholder="Email"><br>
-    <input type="password" v-model="password" placeholder="Password"><br>
-    <button @click="login">Connection</button>
-    <p>You don't have an account ? You can <router-link to="/sign-up">create one</router-link></p>
+    <input
+      type="text"
+      v-model="email"
+      placeholder="Email"
+    >
+    <br>
+    <input
+      type="password"
+      v-model="password"
+      placeholder="Password"
+    >
+    <br>
+    <button @click="login">
+      Connection
+    </button>
+    <p>
+      You don't have an account ? You can
+      <router-link to="/sign-up">
+        create one
+      </router-link>
+    </p>
   </div>
 </template>
 
 <script>
-  import firebase from 'firebase';
+import firebase from 'firebase'
 
-  export default {
-    name: 'login',
-    data() {
-      return {
-        email: '',
-        password: ''
-      }
-    },
-    methods: {
-      login: function() {
-        firebase.auth().signInWithEmailAndPassword(this.email, this.password).then(
-          (user) => {
+export default {
+  name: 'Login',
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    login: function () {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.email, this.password)
+        .then(
+          user => {
             this.$router.replace('home')
           },
-          (err) => {
-            alert('Oops. ' + err.message)
+          err => {
+            throw Error(`Oops. ${err.message}`)
           }
-        );
-      }
+        )
     }
   }
+}
 </script>
 
-<style scoped>  /* "scoped" attribute limit the CSS to this component only */
-  .login {
-    margin-top: 40px;
-  }
-  input {
-    margin: 10px 0;
-    width: 20%;
-    padding: 15px;
-  }
-  button {
-    margin-top: 20px;
-    width: 10%;
-    cursor: pointer;
-  }
-  p {
-    margin-top: 40px;
-    font-size: 13px;
-  }
-  p a {
-    text-decoration: underline;
-    cursor: pointer;
-  }
+<style scoped>
+/* "scoped" attribute limit the CSS to this component only */
+.login {
+  margin-top: 40px;
+}
+input {
+  margin: 10px 0;
+  width: 20%;
+  padding: 15px;
+}
+button {
+  margin-top: 20px;
+  width: 10%;
+  cursor: pointer;
+}
+p {
+  margin-top: 40px;
+  font-size: 13px;
+}
+p a {
+  text-decoration: underline;
+  cursor: pointer;
+}
 </style>
