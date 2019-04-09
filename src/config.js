4e882434f322f01a2487a23671cd547828d9ab41
luebiff <anders.wikrydberg@student.kyh.se>
2019-04-09 13:55:34 +0200

    .

---
+++ src/config.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+const config = {
+  apiKey: 'AIzaSyD50HAHTNxNVi5mhLISv5qMQq5maA8T8PA',
+  authDomain: 'proud-e4833.firebaseapp.com',
+  databaseURL: 'https://proud-e4833.firebaseio.com',
+  projectId: 'proud-e4833',
+  storageBucket: 'proud-e4833.appspot.com',
+  messagingSenderId: '667017411392'
+}
+
+export default config
